a82b474bf9981eb6dbe2d17bb44f31f8b0336f46
michaeadi <adinebomichael@gmail.com>
2022-04-01 19:34:44 +0100

    create the test structure

---
+++ src/test/java/webautomationx/ApiAutomationTest.java
@@ -0,0 +1,14 @@
+package webautomationx;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+public class ApiAutomationTest {
+
+    @Test
+    void usersCreationTest() {
+
+    }
+
+}
