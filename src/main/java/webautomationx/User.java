545e3fca26adbe208ca937e4b45916f1d7de1cb5
michaeadi <adinebomichael@gmail.com>
2022-04-01 19:42:19 +0100

    create the User class and test read from json

---
+++ src/main/java/webautomationx/User.java
@@ -0,0 +1,23 @@
+package webautomationx;
+
+public class User {
+
+    String job;
+    String name;
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/test/java/webautomationx/ApiAutomationTest.java
+++ src/test/java/webautomationx/ApiAutomationTest.java
@@ -1,14 +1,35 @@
 package webautomationx;
 
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 public class ApiAutomationTest {
 
-    @Test
-    void usersCreationTest() {
+    static Gson gson;
 
+    @BeforeAll
+    static void setup() {
+        gson = new Gson();
+    }
+
+    @Test
+    void usersCreationTest() throws UnsupportedEncodingException {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("users.json");
+        Reader reader = new InputStreamReader(in, "UTF-8");
+        User[] users = gson.fromJson(reader, User[].class);
+
+        for (User user : users) {
+            System.out.println(user.name + " - " + user.job);
+        }
     }
 
 }
